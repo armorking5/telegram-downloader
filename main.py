76a5e3c2fdaa2198af87bd13c5e42bed9ca99d5b
armorking5 <11178445+armorking5@users.noreply.github.com>
2017-11-21 16:23:15 +0100

    Test for the previous commit

--- main.py
+++ main.py
@@ -7,21 +7,17 @@ const.ME = 0
 # PARSER
 # Creo il parser degli argomenti con tutte le opzioni del caso
 parser = argparse.ArgumentParser(description="Downloads by default 50 files from the history of a chat in Telegram")
-parser.add_argument("-u","--user",dest="user", help="Username of the chats from where to begin to download files, by default will be used the logged username", default=const.ME)
-parser.add_argument('-b')
-parser.add_argument("-p", "--path", dest="exec_path", help="executible path of telegram-cli if is not on PATH",default=const.DEFAULT_PATH)
+parser.add_argument("-u","--user",dest="user", help="Username of the chats from where to begin to download files, by default will be used the logged username", default='')
+parser.add_argument("-p", "--path", dest="exec_path", help="executible path of telegram-cli if is not on PATH", default='')
 parser.add_argument("-v", "--verbose", action="store_true", dest="verbose", default=False, help="print info messages")
 
 args = parser.parse_args()
 
-telegram = TG(args.exec_path)
+telegram = TG(path=args.exec_path)
 
-cmd = '$path -C -R -N --permanent-msg-ids -W --json -e "history $USER 50"'
-
-if args.user == 0:
-	user = telegram.get_main_user()
+if args.user is not '':
+	user = user.strip(' \t\n\r')
 else:
-	user = args.user
+	user = telegram.get_main_user()
 
-
-Template(cmd).substitute(path=args.path,USER=user)
+telegram.exec('history $USER 50')
